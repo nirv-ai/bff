9a09f76e0239733526dd3c40dbdea63e93e87628
noahehall <noahehall@users.noreply.github.com>
2022-10-28 23:10:40 -0700

    implement pg queries for player play and join routes

--- src/Routers/Player/Routes/PlayerJoin.ts
+++ src/Routers/Player/Routes/PlayerJoin.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { validationResult } from "express-validator";
 
+import { nirvDbCore, createPgQuery } from "../../../Data";
 import type { PlayerDataType } from "../../../Types";
 
 export const PlayerJoinRoute = async (req: Request, res: Response) => {
@@ -9,10 +10,28 @@ export const PlayerJoinRoute = async (req: Request, res: Response) => {
   if (!errors.isEmpty())
     return res.status(400).json({ errors: errors.array() });
 
-  // TODO: we are just sending back whatever we receive
-  await Promise.resolve();
+  const { callsign, email, password, avatar, first, last, about } =
+    req.body as PlayerDataType;
 
-  const { password, ...player } = req.body as PlayerDataType;
+  const createPlayerQuery = createPgQuery({
+    text: `
+      insert into nirvai.players (
+        callsign, email, password, avatar, first, last, about
+      ) values (
+        $1, $2, nirvai.crypt($3, nirvai.gen_salt('bf')), $4, $5, $6, $7
+      )
+      returning *
+    `,
+    values: [callsign, email, password, avatar, first, last, about],
+  });
+
+  const newPlayer: PlayerDataType | null = await nirvDbCore.one(
+    createPlayerQuery
+  );
+
+  if (!newPlayer) return res.status(400).json({});
+
+  const { password: ignorePassword, ...player } = newPlayer;
 
   return res.json({ player });
 };
